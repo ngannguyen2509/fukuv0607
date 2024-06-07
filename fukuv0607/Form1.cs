900d3ef24bf9a0651205d3a9f50a42c4c93aab45
hori akari <104044168+ruthia-1341@users.noreply.github.com>
2024-06-07 13:38:09 +0900

    fukushyu

--- fukuv0607/Form1.cs
+++ fukuv0607/Form1.cs
@@ -2,9 +2,38 @@ namespace fukuv0607
 {
     public partial class Form1 : Form
     {
+        int vx = -4;
+        int vy = -4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("ngan");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+
+                vx = 10;
+
+            if (label1.Top < 0)
+
+                vy = 10;
+
+            if (label1.Left > 692)
+                vx = -10;
+
+            if (label1.Top > 692)
+                vx = -10;
+
         }
     }
 }
